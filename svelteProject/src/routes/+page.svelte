dff700e3b71f6630c1538beab29dff6eee947044
dlgudw123 <dlgudwn123123@gmail.com>
2023-02-04 16:36:00 +0900

    test

--- svelteProject/src/routes/+page.svelte
+++ svelteProject/src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let name = "이형주";
+</script>
+
+<h1>Svelte <a href="/apple">to apple</a></h1>
